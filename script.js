2535377b2feb4f020f68f23ed538fe00cb41ec90
Rafael Saito <107894019+rafaelsaito@users.noreply.github.com>
2025-07-01 14:38:10 -0300

    x

--- script.js
+++ script.js
@@ -10,3 +10,6 @@ let linguagens = ["JavaScript", "Python", "C"];
 console.log(linguagens[0]) // “JavaScript”
 console.log(linguagens[1]) // “Python”
 console.log(linguagens[2]) // “C”
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0];
