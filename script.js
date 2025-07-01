0a7f008af8d8b56007964b95657c44fc3ceb0f26
Rafael Saito <107894019+rafaelsaito@users.noreply.github.com>
2025-07-01 14:40:39 -0300

    \

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function inserirNome () {
     let nomeUsuario = prompt ("Qual o seu nome");
     let elemento = document.querySelector("#nome-usuario");
+    console.log (elemento);
     elemento.textContent = nomeUsuario;
 }
 
@@ -13,3 +14,5 @@ console.log(linguagens[2]) // “C”
 
 const item = document.querySelector("#lista");
 item.textContent = linguagens[0];
+item.textContent = linguagens[1];
+item.textContent = linguagens[2];
