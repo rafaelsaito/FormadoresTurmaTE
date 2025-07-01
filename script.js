6c5ddc10f77ea2a51069ed29e887c2f8b09fd2ac
Rafael Saito <107894019+rafaelsaito@users.noreply.github.com>
2025-07-01 14:37:00 -0300

    a

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function inserirNome () {
 }
 
 inserirNome();
+
+let linguagens = ["JavaScript", "Python", "C"];
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
